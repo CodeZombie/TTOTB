5fa47843f04e9d67603745cc36485a7f599b7df6
Jeremy Clark <jeremyclark@live.ca>
2020-05-19 22:01:42 -0400

    Begining decoupling

--- Enemy.gd
+++ Enemy.gd
@@ -1,7 +1,13 @@
 extends KinematicBody2D
 class_name Enemy
+signal hurt
+
 onready var ai_fsm =  FiniteStateMachine.new()
 onready var Fireball = preload("res://Fireball.tscn")
+onready var vision = get_node("Vision")
+var node_in_vision = null
+var node_to_follow = null
+const FOLLOW_DISTANCE = 256
 const GRAVITY = 800
 const WALK_SPEED = 100
 const JUMP_SPEED = 250
@@ -41,33 +47,29 @@ func state_attack_player() -> void:
 		fireball.position = position
 		get_tree().get_root().get_node("Scene").add_child(fireball)
 
-func can_see_player() -> bool:
-	var sight_ray = get_node("SightRay")
-	sight_ray.cast_to(Vector2(0, 50))
-
-	#sight_ray.cast_to(get_node("../MainCharacter").position)
-	if(sight_ray.get_collider().is_in_group("player")):
-		return true
-	return false
-	
-
 func condition_near_player() -> bool:
-	var mc = get_node("../MainCharacter")
-	var mc_pos = mc.get_position()
-	var distance = mc_pos.distance_to(self.get_position())
-	if(distance < 128):
+	if(node_in_vision != null):
+		node_to_follow = node_in_vision
 		return true
 	return false
 
 func condition_not_near_player() -> bool:
-	return not condition_near_player()
+	if(node_to_follow != null):
+		if(get_global_position().distance_to(node_to_follow.get_global_position()) >= FOLLOW_DISTANCE):
+			node_to_follow = null
+			return true
+		return false
+	else:
+		return true
+	#return not condition_near_player()
 
 func condition_player_in_attack_range() -> bool:
 	var mc = get_node("../MainCharacter")
-	var mc_pos = mc.get_position()
-	var distance = mc_pos.distance_to(self.get_position())
-	if(distance < 64):
-		return true
+	if(mc != null):
+		var mc_pos = mc.get_position()
+		var distance = mc_pos.distance_to(self.get_position())
+		if(distance < 64):
+			return true
 	return false
 
 func condition_player_not_in_attack_range() -> bool:
@@ -83,7 +85,12 @@ func _ready():
 	ai_fsm.register_transition(self, "follow_player", "attack_player", "condition_player_in_attack_range")
 	ai_fsm.register_transition(self, "attack_player", "follow_player", "condition_player_not_in_attack_range")
 	ai_fsm.set_default_state("wander")
-	pass
+	
+	#Signals
+	connect("hurt", self, "hurt")
+
+func hurt():
+	print("I AM HURT")
 
 func handle_animation():
 	if(velocity.x > 0):
@@ -92,7 +99,16 @@ func handle_animation():
 		get_node("AnimatedSprite").set_flip_h(true)
 		
 func _process(delta):
+	#print(" Near Body body?: %b", condition_near_player())
 	velocity.y += delta * GRAVITY
 	ai_fsm.update()
 	velocity = move_and_slide(velocity, Vector2(0, -1))
 	handle_animation()
+
+func _on_Vision_body_entered(body):
+	node_in_vision = body
+
+func _on_Vision_body_exited(body):
+	if(node_in_vision != null):
+		if(body.get_instance_id() == node_in_vision.get_instance_id()):
+			node_in_vision = null
